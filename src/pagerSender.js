ec99f8446954bae82bc46efcf31f0ba715fce1fa
alex_sze <alexander@szeremeta.org>
2026-01-18 22:09:27 -0500

    Delete pagerSender.js

--- src/pagerSender.js
+++
@@ -1,6 +0,0 @@
-// Browser delivery removed; keep stub to guard against accidental usage.
-async function sendToPager() {
-  throw new Error('Browser delivery was removed; use the HTTP API sender instead.');
-}
-
-module.exports = { sendToPager };
